3a395f9084d116688b4512b4f3cf2303e7471ce7
Sofia Papagiannaki <spapagian@ics.forth.gr>
2015-10-26 19:45:51 +0200

    Lighter nested implementation for peak detection.

--- src/main/scala/PeakDetectionMapBlockLight.scala
+++ src/main/scala/PeakDetectionMapBlockLight.scala
@@ -12,19 +12,20 @@ import org.apache.spark.rdd.RDD
 
 import org.joda.time.format.DateTimeFormat
 
-object PeakDetectionMapBlock extends PeakDetectionNested{
+object PeakDetectionMapBlockLight extends PeakDetectionNested{
   override def calcDataRaws(cdr: RDD[_]) = {
-    val tmp = cdr.mapBlock( (_, array1) => {
-      cdr.mapBlock( (_, array2) => {
-        array1.map(e => {
-          val num = array2.filter(e2 => {
-            e == e2
-          }).size
-          (e, num)
-        })
-      }).collect()
+    val tmp = cdr.mapBlock( (i, array1) => {
+      val a1 = array1.groupBy(e => e).map{ case (k, v) => (k, v.size) }
+      println(s"-->array1: ${array1.mkString("\n")}, a1: ${a1.mkString("\n")}}")
+      cdr.mapBlock( (j, array2) => {
+        val m = if (i < j) {
+          val a2 = array2.groupBy(e => e).map{ case (k, v) => (k, v.size) }
+          a1 ++ a2.map{ case (k, v) => k -> (v + a1.getOrElse(k, 0)) }
+        } else Map()
+        m.toArray
+      }).collect
     })
-    tmp.distinct.reduceByKey(_+_).map{ case ((id:String, hour:Int, dow:Int, doy:Int), num: Int) =>
+    tmp.map{ case ((id:String, hour:Int, dow:Int, doy:Int), num: Int) =>
       new DataRaw(id, hour, dow, doy, num)
     }
   }
