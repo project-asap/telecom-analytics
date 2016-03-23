be72f9a6fa0f14ddb90feb4798edc4a0c0d4377d
Sofia Papagiannaki <spapagian@ics.forth.gr>
2016-03-23 15:39:30 +0100

    Minor modifications in visualization

--- src/python/visualization/stats_publisher.py
+++ src/python/visualization/stats_publisher.py
@@ -25,31 +25,29 @@ spatial_division = sys.argv[1]
 region = sys.argv[2]
 timeframe = sys.argv[3]
 
-geo = open('roma_gsm.csv')
-coord = {x.split(";")[0][:5]: (x.split(";")[1:]) for x in geo.readlines()}
+with open('roma_gsm.csv') as geo:
+    coord = {x.split(";")[0][:5]: (x.split(";")[1:]) for x in geo.readlines()}
 
-peaks = open('../statistics/timeseries%s-%s-%s.csv' %
-             (region, timeframe, spatial_division.split("/")[-1]))
+with open('../statistics/timeseries%s-%s-%s.csv' %
+          (region, timeframe, spatial_division.split("/")[-1])) as peaks:
+    obs = []
 
+    for i, p in enumerate(peaks.readlines()):
+        s = p.split(",")
+        target_location = s[0]
+        loc = coord[s[0]]
+        value = s[-1]
+        date = d = datetime.datetime.strptime(s[1] + s[2], ' %Y-%m-%d %H')
+        d = {}
+        d["_id"] = i
+        d["value"] = str(value).strip("\n")
+        d["date"] = str(date)
+        d["target_location"] = [{"name": target_location, "point": {
+            "lat": loc[0], "lon":loc[1].strip("\n")}}]
+        d["indicator_id"] = "hourly presence"
+        d["indicator_name"] = "hourly presence"
+        obs.append(d)
 
-obs = []
-
-for i, p in enumerate(peaks.readlines()):
-    s = p.split(",")
-    target_location = s[0]
-    loc = coord[s[0]]
-    value = s[-1]
-    date = d = datetime.datetime.strptime(s[1] + s[2], ' %Y-%m-%d %H')
-    d = {}
-    d["_id"] = i
-    d["value"] = str(value).strip("\n")
-    d["date"] = str(date)
-    d["target_location"] = [{"name": target_location, "point": {
-        "lat": loc[0], "lon":loc[1].strip("\n")}}]
-    d["indicator_id"] = "hourly presence"
-    d["indicator_name"] = "hourly presence"
-    obs.append(d)
-
-file = open("wl_timeseries%s-%s-%sarea_presence.json" %
-            (region, timeframe, spatial_division.split("/")[-1]), "w")
-json.dump(obs, file)
+with open("wl_timeseries%s-%s-%sarea_presence.json" %
+          (region, timeframe, spatial_division.split("/")[-1]), "w") as file:
+    json.dump(obs, file)
--- src/python/visualization/weblyzard_api.py
+++ src/python/visualization/weblyzard_api.py
@@ -6,29 +6,28 @@ spatial_division = sys.argv[1]
 region = sys.argv[2]
 timeframe = sys.argv[3]
 
-geo = open('roma_gsm.csv')
-coord = {x.split(";")[0][:5]: (x.split(";")[1:]) for x in geo.readlines()}
+with open('roma_gsm.csv') as geo:
+    coord = {x.split(";")[0][:5]: (x.split(";")[1:]) for x in geo.readlines()}
 
-peaks = open('../statistics/timeseries%s-%s-%s.csv' %
-             (region, timeframe, spatial_division.split("/")[-1]))
+with open('../statistics/timeseries%s-%s-%s.csv' %
+          (region, timeframe, spatial_division.split("/")[-1])) as peaks:
+    obs = []
 
-obs = []
+    for i, p in enumerate(peaks.readlines()):
+        s = p.split(",")
+        target_location = s[0]
+        loc = coord[s[0]]
+        value = s[-1]
+        date = d = datetime.datetime.strptime(s[1] + s[2], ' %Y-%m-%d %H')
+        d = {}
+        d["_id"] = i
+        d["value"] = str(value).strip("\n")
+        d["date"] = str(date)
+        d["target_location"] = [{"name": target_location, "point": {
+            "lat": loc[0], "lon":loc[1].strip("\n")}}]
+        d["indicator_id"] = "hourly presence"
+        d["indicator_name"] = "hourly presence"
+        obs.append(d)
 
-for i, p in enumerate(peaks.readlines()):
-    s = p.split(",")
-    target_location = s[0]
-    loc = coord[s[0]]
-    value = s[-1]
-    date = d = datetime.datetime.strptime(s[1] + s[2], ' %Y-%m-%d %H')
-    d = {}
-    d["_id"] = i
-    d["value"] = str(value).strip("\n")
-    d["date"] = str(date)
-    d["target_location"] = [{"name": target_location, "point": {
-        "lat": loc[0], "lon":loc[1].strip("\n")}}]
-    d["indicator_id"] = "hourly presence"
-    d["indicator_name"] = "hourly presence"
-    obs.append(d)
-
-file = open("wl_area_presence.json", "w")
-json.dump(obs, file)
+with open("wl_area_presence.json", "w") as file:
+    json.dump(obs, file)
