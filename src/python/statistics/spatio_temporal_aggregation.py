ac32fb7012aa9021c623195fbb46db28e32ba224
Sofia Papagiannaki <spapagian@ics.forth.gr>
2016-12-09 13:16:09 +0200

    Spatio-temporal aggregation: improve code readability

--- src/python/statistics/spatio_temporal_aggregation.py
+++ src/python/statistics/spatio_temporal_aggregation.py
@@ -1,8 +1,12 @@
 __author__ = 'paul'
-import datetime
-from pyspark import SparkContext, StorageLevel
-
+import string
 import sys
+
+from itertools import imap
+from cdr import CDR
+from utils import quiet_logs
+
+from pyspark import SparkContext, StorageLevel
 
 """
 Spatio-temporal Aggregation module
@@ -21,47 +25,16 @@ Results are stored into file: timeseries-<region>-<timeframe>-<spatial_division>
 
 """
 
-
-########################functions##################################
-def quiet_logs(sc):
-    logger = sc._jvm.org.apache.log4j
-    logger.LogManager.getLogger("org").setLevel(logger.Level.ERROR)
-    logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
-
-
-def validate(date_text, date_format='%Y-%m-%d %X'):
-    # if the string is a date, return True (useful to filter csv header)
-    date_text = date_text.strip()
-    try:
-        datetime.datetime.strptime(date_text, date_format)
-        return True
-    except ValueError:
-        return False
-
-
-def municipio(cell_id, cell2municipi):
-    try:
-        cell2municipi[cell_id]
-        return True
-    except KeyError:
-        return False
-
-
-##########################################################################
 folder = sys.argv[1]
 spatial_division = sys.argv[2]
 region = sys.argv[3]
 timeframe = sys.argv[4]
 
 # spatial division: cell_id->region of interest
-
-file = open(spatial_division)
-# converting cell to municipality
-cell2municipi = {
-    k: v for k, v in [
-        (x.split(';')[0].replace(
-            " ", ""), x.split(';')[1].replace(
-                "\n", "")) for x in file.readlines()]}
+with open(spatial_division) as file:
+    # converting cell to municipality
+    cell2region = {k: v for k, v in [
+        imap(string.strip, x.split(';')) for x in file.readlines()]}
 
 sc = SparkContext()
 quiet_logs(sc)
@@ -70,15 +43,18 @@ peaks = open('timeseries-%s-%s-%s' %
              (region, timeframe, spatial_division.split("/")[-1]), 'w')
 
 lines = sc.textFile(folder) \
-    .filter(lambda x: validate(x.split(';')[3])) \
-    .filter(lambda x: municipio(x.split(';')[9].replace(" ", ""), cell2municipi)) \
-    .map(lambda x: (x.split(';')[0], cell2municipi[x.split(';')[9].replace(" ", "")], x.split(';')[3], x.split(';')[4][:2] )) \
+    .map(lambda x: CDR.from_string(x)) \
+    .filter(lambda x: x is not None) \
+    .filter(lambda x: x.valid_region(cell2region)) \
+    .map(lambda x: (x.user_id, x.region(cell2region), x.date, x.time[:2] )) \
     .distinct() \
     .persist(StorageLevel(False, True, False, False))
 
 chiamate_orarie = lines.map(
-    lambda x: (
-        (x[1], x[2], x[3]), 1)).reduceByKey(
-            lambda x, y: x + y)
-for l in chiamate_orarie.collect():
-    print >>peaks, "%s,%s,%s,%s" % (l[0][0], l[0][1], l[0][2], l[1])
+    lambda __region_date_time: (
+        (__region_date_time[1],
+         __region_date_time[2],
+         __region_date_time[3]),
+        1)).reduceByKey( lambda x, y: x + y)
+for (region, date, time), count in chiamate_orarie.collect():
+    print >>peaks, "%s,%s,%s,%s" % (region, date, time, count)
