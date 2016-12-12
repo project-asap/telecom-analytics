5f1f310d9181f20042866b1374db88c2fff36f4d
Sofia Papagiannaki <spapagian@ics.forth.gr>
2016-12-12 14:55:03 +0200

    Spatio-temporal aggregation: save the start cells

--- src/python/statistics/spatio_temporal_aggregation.py
+++ src/python/statistics/spatio_temporal_aggregation.py
@@ -3,6 +3,8 @@ import string
 import sys
 
 from itertools import imap
+from sets import Set
+
 from cdr import CDR
 from utils import quiet_logs
 
@@ -10,12 +12,12 @@ from pyspark import SparkContext, StorageLevel
 
 """Spatio-temporal Aggregation module.
 
-Given a CDR dataset and a set of geographical regions, it returns presences timeseries for each spatial region.
+Given a CDR dataset and a set of geographical regions, it returns presence timeseries for each spatial region.
 More specifically the results are tuples containing the following information:
-<region>,<date>,<time>,<count>
+<region>,<date>,<time>,<count>,<start-cells>
 
 Usage:
-    $SPARK_HOME/bin/spark-submit --py-files cdr.py statistics/spatio_temporal_aggreation.py \
+    $SPARK_HOME/bin/spark-submit --py-files cdr.py,utils.py statistics/spatio_temporal_aggregation.py \
 <dataset> <spatial_division> <region> <timeframe>
 
 Args:
@@ -27,13 +29,15 @@ Args:
     region: The region name featuring in the stored results
     timeframe: The timeframe featuring in the stored results
 
-Results are stored into the local file: timeseries-<region>-<timeframe>
-where the <region> is derived by the spatial_division.
+Results are stored into the local file: timeseries-<region>-<timeframe>.
 
 Example:
-    $SPARK_HOME/bin/spark-submit --py-files cdr.py statistics/spatio_temporal_aggreation.py \
+    $SPARK_HOME/bin/spark-submit --py-files cdr.py,utils.py statistics/spatio_temporal_aggregation.py \
 hdfs:///dataset_simulated/2016 spatial_regions/aree_roma.csv roma 2016
 """
+
+def region_coordinations(region, cell2region, cell2coordinates):
+    points = [cell2coordinates(k) for k in cell2region if cell2region[k] == region]
 
 folder = sys.argv[1]
 spatial_division = sys.argv[2]
@@ -41,10 +45,10 @@ region = sys.argv[3]
 timeframe = sys.argv[4]
 
 # spatial division: cell_id->region of interest
-with open(spatial_division) as file:
+with open(spatial_division) as f:
     # converting cell to municipality
     cell2region = {k: v for k, v in [
-        imap(string.strip, x.split(';')) for x in file.readlines()]}
+        imap(string.strip, x.split(';')) for x in f.readlines()]}
 
 sc = SparkContext()
 quiet_logs(sc)
@@ -55,15 +59,13 @@ lines = sc.textFile(folder) \
     .map(lambda x: CDR.from_string(x)) \
     .filter(lambda x: x is not None) \
     .filter(lambda x: x.valid_region(cell2region)) \
-    .map(lambda x: (x.user_id, x.region(cell2region), x.date, x.time[:2] )) \
+    .map(lambda x: (x.user_id, x.start_cell, x.region(cell2region), x.date, x.time[:2] )) \
     .distinct() \
     .persist(StorageLevel(False, True, False, False))
 
-chiamate_orarie = lines.map(
-    lambda __region_date_time: (
-        (__region_date_time[1],
-         __region_date_time[2],
-         __region_date_time[3]),
-        1)).reduceByKey( lambda x, y: x + y)
-for (region, date, time), count in chiamate_orarie.collect():
-    print >>peaks, "%s,%s,%s,%s" % (region, date, time, count)
+chiamate_orarie = lines.keyBy(lambda x: x[2:])\
+    .map(lambda (k, (_1, cell, _2, _3, _4)): (k, (1, Set([cell]))))\
+    .reduceByKey(lambda x, y: (x[0] + y[0], x[1] | y[1]))
+
+for ((region, date, time), (count, cells)) in chiamate_orarie.collect():
+    print >>peaks, "%s,%s,%s,%s,%s" % (region, date, time, count, cells)
