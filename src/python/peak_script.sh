0cb39008c6f524a8846364fabe18ac28e5b363ad
Sofia Papagiannaki <spapagian@ics.forth.gr>
2017-03-28 16:07:33 +0300

    Delete outdated python modules

--- src/python/peak_script.sh
+++
@@ -1,6 +0,0 @@
-DATASET=$1
-SPARK_MASTER=$2
-export PYTHONPATH=.:$PYTHONPATH
-$SPARK_HOME/bin/spark-submit --py-files cdr.py --master $SPARK_MASTER peak_detection/data_filter.py $DATASET spatial_regions/aree_roma.csv roma 2016-01-01 2016-01-31
-$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER peak_detection/typical_distribution_computation.py /peaks/hourly_presence roma 2016-01-01 2016-01-31
-$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER peak_detection/peak_detection.py /peaks/hourly_presence /peaks/weekly_presence roma 2016-01-01 2016-01-31
